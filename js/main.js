9fdb92a8394c3ce54056ee668842af1f4d0ff0c6
Felipe Garnica Hernández <jhoanfelipe1919@gmail.com>
2023-05-15 09:05:39 -0500

    Se añadio CSS

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//comentario
+
+//variables
+/*Son espacios de almacenamiento de informacion en el caso de las const por lo general se crean en mayuscula y no se pueden redefinir*/
+var nombre;
+let nombre;
+const NOMBRE = "Felipe";
+//tipos de Datos
+/*String: "Hola" o 'Hola' o ´hola´
+Number: 1 o 1.5
+Boolean:('') true o false
+Object: Object({})({} {})
+Array: Array() Array.from({}) 0 []
+Undefined
+Null
+NaN: 4 - "a"
+Promise
+Class: POO
+Functions: nombre(argumentos){
+  return
+}
+
+const NOMBRE = funcion(){}
+nombre = argumento =>{
+  return
+}
+*/
+//Operadores Aritmeticos
+/*
++ suma
++= adicion o incremento
+- resta 
+-= sustraccion o decremento
+/ division
+% resto o modulo
+Math.sqrt()
+Math.random()
+Math.abs()
+
+*/
+//Operaciones Logicas
+
+//Condicionales
+
+//Funciones
